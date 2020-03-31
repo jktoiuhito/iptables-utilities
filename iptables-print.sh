ff65e55982de3b3e519890f86fba8b3f6108f8ab
jktoiuhito <56585957+jktoiuhito@users.noreply.github.com>
2020-03-31 17:35:56 +0300

    made quirk-proofier

--- iptables-print.sh
+++ iptables-print.sh
@@ -1,37 +1,66 @@
 #!/bin/sh
 clear
 echo "PREROUTING"
-echo "\nraw"
+echo
+echo "raw"
+echo
 iptables -t raw -L PREROUTING -n -v
-echo "\nmangle"
+echo
+echo "mangle"
+echo
 iptables -t mangle -L PREROUTING -n -v
-echo "\nnat"
+echo
+echo "nat"
+echo
 iptables -t nat -L PREROUTING -n -v
-#
-echo "\n\nINPUT"
-echo "\nmangle"
+echo
+echo
+echo "INPUT"
+echo
+echo "mangle"
+echo
 iptables -t mangle -L INPUT -n -v
-echo "\nfilter"
+echo
+echo "filter"
+echo
 iptables -t filter -L INPUT -n -v
-#
-echo "\n\nFORWARD"
-echo "\nmangle"
+echo
+echo
+echo "FORWARD"
+echo
+echo "mangle"
+echo
 iptables -t filter -L FORWARD -n -v
-echo "\nfilter"
+echo
+echo "filter"
 iptables -t filter -L FORWARD -n -v
-#
-echo "\n\nOUTPUT"
-echo "\nraw"
+echo
+echo
+echo "OUTPUT"
+echo
+echo "raw"
+echo
 iptables -t nat -L OUTPUT -n -v
-echo "\nmangle"
+echo
+echo "mangle"
+echo
 iptables -t nat -L OUTPUT -n -v
-echo "\nnat"
+echo
+echo "nat"
+echo
 iptables -t nat -L OUTPUT -n -v
-echo "\nfilter"
+echo
+echo "filter"
+echo
 iptables -t nat -L OUTPUT -n -v
-#
-echo "\n\nPOSTROUTING"
-echo "\nmangle"
+echo
+echo
+echo "POSTROUTING"
+echo
+echo "mangle"
+echo
 iptables -t nat -L POSTROUTING -n -v
-echo "\nnat"
+echo
+echo "nat"
+echo
 iptables -t nat -L POSTROUTING -n -v

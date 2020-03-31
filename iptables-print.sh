6aff8484ed231a438c3b6b5beeb4d32e742e9316
jktoiuhito <56585957+jktoiuhito@users.noreply.github.com>
2020-03-31 18:04:52 +0300

    made output prettier

--- iptables-print.sh
+++ iptables-print.sh
@@ -1,66 +1,58 @@
 #!/bin/sh
 clear
-echo "PREROUTING"
+echo
+echo "------------------------------ iptables-print.sh ------------------------------"
+echo "                        `date`"
+echo
+echo "                       ---------- PREROUTING ----------"
 echo
 echo "raw"
-echo
 iptables -t raw -L PREROUTING -n -v
 echo
 echo "mangle"
-echo
 iptables -t mangle -L PREROUTING -n -v
 echo
 echo "nat"
-echo
 iptables -t nat -L PREROUTING -n -v
 echo
 echo
-echo "INPUT"
+echo "                         ---------- INPUT ----------"
 echo
 echo "mangle"
-echo
 iptables -t mangle -L INPUT -n -v
 echo
 echo "filter"
-echo
 iptables -t filter -L INPUT -n -v
 echo
 echo
-echo "FORWARD"
+echo "                        ---------- FORWARD ----------"
 echo
 echo "mangle"
-echo
 iptables -t filter -L FORWARD -n -v
 echo
 echo "filter"
 iptables -t filter -L FORWARD -n -v
 echo
 echo
-echo "OUTPUT"
+echo "                         ---------- OUTPUT ----------"
 echo
 echo "raw"
-echo
 iptables -t nat -L OUTPUT -n -v
 echo
 echo "mangle"
-echo
 iptables -t nat -L OUTPUT -n -v
 echo
 echo "nat"
-echo
 iptables -t nat -L OUTPUT -n -v
 echo
 echo "filter"
-echo
 iptables -t nat -L OUTPUT -n -v
 echo
 echo
-echo "POSTROUTING"
+echo "                       ---------- POSTROUTING ----------"
 echo
 echo "mangle"
-echo
 iptables -t nat -L POSTROUTING -n -v
 echo
 echo "nat"
-echo
 iptables -t nat -L POSTROUTING -n -v
